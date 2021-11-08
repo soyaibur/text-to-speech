e38e9ce935f639fbed62cc15994ab9eb9ebd5b27
soyaibur <ovisoyaiburrahman@gmail.com>
2021-11-08 18:38:16 +0600

    Requered Elements Selected

---
+++ logic.js
@@ -0,0 +1,11 @@
+const play = document.getElementById('play-btn')
+const TextInput = document.getElementById('text')
+const pause = document.getElementById('pause-btn')
+const stop = document.getElementById('stop-btn')
+const speed = document.getElementById('speed')
+
+
+
+addEventListener('click',()=>{
+    alert("this is working")
+})
